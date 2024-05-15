01fc50bfba6d4cec794e8136b55882be1d22a8b1
lucas <124883077+lucasszx@users.noreply.github.com>
2024-05-15 16:26:37 -0300

    atualizado

--- Script.js
+++ Script.js
@@ -1,17 +1,22 @@
-function clicou(){
-    let clic = document.getElementById("daf")
-    let show = document.getElementById("cil")
-    let show2 = document.getElementById("gil")
-    
-    if (show.style.display === "block" && show2.style.display === "block") {
+function clicou(event) {
+  let show = document.getElementById("cil");
+  let show2 = document.getElementById("gil");
+  if (show.style.display === "block" && show2.style.display === "block") {
     show.style.display = "none";
     show2.style.display = "none";
   } else {
     show.style.display = "block";
     show2.style.display = "block";
-  
   }
+  event.stopPropagation();
+}
+
+function clicou2(event) {
+  let info = event.currentTarget.querySelector("p");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
   }
-  
-  
-  
+  event.stopPropagation();
+}
